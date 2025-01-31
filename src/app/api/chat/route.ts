c0a3b31ae62ecc21022e20f664eb26507129d467
emmanuel <chifez1@gmail.com>
2025-02-01 00:22:11 +0100

    starter template fully restructured

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+import { tools } from '../../ai/tools';
+
+export async function POST(request: Request) {
+  const { messages } = await request.json();
+
+  const result = await streamText({
+    model: openai('gpt-4o'),
+    system: 'You are a friendly assistant!',
+    messages,
+    maxSteps: 5,
+    tools,
+  });
+
+  return result.toDataStreamResponse();
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Header } from "@/components/header";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Next.js AI Lite App",
-  description: "AI with Next and AI SDK",
+  title: 'Next.js AI Lite App',
+  description: 'AI with Next and AI SDK',
 };
 
 export default function RootLayout({
@@ -18,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
         <main className="bg-muted/50 flex h-100vh flex-1 flex-col">
           {children}
         </main>
